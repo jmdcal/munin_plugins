7a617912401e7c1851de364d3e5c5614f6f5a1a8
Federico C. Guizzardi <cippinofg@gmail.com>
2013-07-17 15:03:05 +0200

    Refactor and fixes

--- monit_downtime.py
+++ monit_downtime.py
@@ -19,49 +19,59 @@ def print_config(title,group):
   print "failedtest.draw AREASTACK"
   print "failedtest.colour FF0000"
 
+def lock_fn(fd):
+  #locking file
+  locked=False
+  while not locked:
+    try:
+      fcntl.lockf(fd,fcntl.LOCK_EX)
+    except IOError:
+      time.sleep(3)
+    else:
+      locked=True
+
+def unlock_fn(df)
+  fcntl.lockf(fd, fcntl.LOCK_UN)
+  
+def open_and_update(fn):
+  count=0
+  #open file to check how many time
+  try:
+    #it exists
+    fd=open(fn,'r+')
+  except IOError:
+    #it not exists
+    fd=open(fn,'w')
+  
+  lock_fn(fd)
+
+  #try to get previous value
+  try:
+    count=int(fd.read())
+  except IOError,ValueError:
+    #IOError means that file is open in W mode
+    #ValueError means that file doesn't contains an int
+    count=0
+    
+  count+=1
+  fd.seek(0)    
+  fd.write(count)
+  fd.truncate()
+  unlock_fn(fd)
+  fd.close()  
+
+  return count
+
 
 if len(sys.argv)>1 and sys.argv[1]=='config':
   print_config('Monit downtime','monit')
 else:
+  count=0
   try:
     pid=int(subprocess.check_output(['pidof','monit']).strip())
   except subprocess.CalledProcessError, ValueError:
-    #open file to check how many time
-    try:
-      #it exists
-      fd=open(DOWNTIME_COUNTER,'r+')
-    except IOError:
-      #it not exists
-      fd=open(DOWNTIME_COUNTER,'w')
-      
-    #locking file
-    locked=False
-    while not locked:
-      try:
-        fcntl.lockf(fd,fcntl.LOCK_EX)
-      except IOError:
-        time.sleep(3)
-      else:
-        locked=True
-    count
-        
-    #try to get previous value
-    try:
-      count=int(fd.read())
-    except IOError,ValueError:
-      #IOError means that file is open in W mode
-      #ValueError means that file doesn't contains an int
-      count=0
-      
-    count+=1
-    
-    fd.seek(0)    
-    fd.write(count)
-    fd.truncate()
-    fd.close()
-  else:
-    #ok we will return 0
-    count=0
+    #if fails means that the process is not running
+    count=open_and_update(DOWNTIME_COUNTER)               
     
   print "failedtest.value %s"%count
   
