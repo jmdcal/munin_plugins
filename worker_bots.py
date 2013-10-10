85d3e0174a2bb2592259e2d3a3d6c6b25b421954
Federico C. Guizzardi <cippinofg@gmail.com>
2013-10-10 15:01:09 +0200

    Addes spider label to catch Baiduspider/2.0 and sons

--- worker_bots.py
+++ worker_bots.py
@@ -26,7 +26,7 @@ def agents_list(access_file,limit):
       if datas.is_valid_line([200,]):
         dt=datas.get_date()
         agent=datas.get_agent()
-        if agent is not None and 'bot' in agent and not WL_AGENTS.search(agent) and dt>limit:
+        if agent is not None and ('bot' in agent or 'spider' in agent) and not WL_AGENTS.search(agent) and dt>limit:
           agent=get_short_agent(agent)
           agents[agent]=1+agents[agent]
   
