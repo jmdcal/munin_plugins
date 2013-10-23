259b29896a886b468e4e16e1e239f678d8ef0734
Federico C. Guizzardi <cippinofg@gmail.com>
2013-10-23 16:44:35 +0200

    Bugfix

--- nginx_full.py
+++ nginx_full.py
@@ -18,7 +18,9 @@ def print_config(id,v):
   print "%s.label %s"%(id,id)
 
 class LatencyAggregator(object):
-  def __init__(self):
+  def __init__(self,title,group):
+    self.title=title
+    self.group=group
     self.counter=Counter(dict([(str(i),0) for i in INTERVALS]+[('others',0)]))
     
   def update_with(self,datas):
@@ -33,15 +35,15 @@ class LatencyAggregator(object):
 
       if pos<len(INTERVALS):
         idx=str(INTERVALS[pos])
-        self.counter[idx]=1+aggr_counters[idx]
+        self.counter[idx]=1+self.counter[idx]
       else:
         self.counter['others']=1+self.counter['others']
         
-  def print_config_header(self,title,group):
-    print "graph_title Nginx latency (new): %s"%title
+  def print_config_header(self):
+    print "graph_title Nginx latency: %s"%self.title
     print "graph_args --base 1000"
     print "graph_vlabel number of pages"
-    print "graph_category %s"%group
+    print "graph_category %s"%self.group
     
   def print_data(self, printer):
     for threshould in INTERVALS:
@@ -54,45 +56,51 @@ class LatencyAggregator(object):
     printer(id,v)
         
 class BotsCounter(object):
-  def __init__(self):
+  def __init__(self,title,group):
+    self.title=title
+    self.group=group
     self.counter=CacheCounter(CACHE_BOTS)
     
-  def update_with(self,datas):
+  def update_with(self,datas):    
     if datas.get_int_code() in [200,]:
       agent=datas.get_agent()
       agent=get_short_agent(agent)
       self.counter[agent]=1+self.counter[agent]
       
-  def print_config_header(self,title,group):
-    print "graph_title Nginx Bots: %s"%title
+  def print_config_header(self):
+    print "graph_title Nginx Bots: %s"%self.title
     print "graph_args --base 1000"
     print "graph_vlabel number of call"
-    print "graph_category %s"%group
+    print "graph_category %s"%self.group
 
   def print_data(self, printer):
-    for l,v in self.counter:
+    for l,v in self.counter.items():
       printer(l,v)
      
 class HttpCodesCounter(object):
-  def __init__(self):
+  def __init__(self,title,group):
+    self.title=title
+    self.group=group
     self.counter=Counter(dict([(str(i),0) for i in HTTP_CODES.keys()]))
     
   def update_with(self,datas):
     if datas.get_int_code() in HTTP_CODES.keys():
+      code=datas.get_code()
       self.counter[code]=self.counter[code]+1
           
-  def print_config_header(self,title,group):    
-    print "graph_title Nginx http codes: %s"%title
+  def print_config_header(self):    
+    print "graph_title Nginx http codes: %s"%self.title
     print "graph_args --base 1000"
     print "graph_vlabel q.ty"
-    print "graph_category %s"%group
+    print "graph_category %s"%self.group
     
   def print_data(self, printer):
-    for k in sorted(HTTP_CODES.keys()):
-      printer("code%s"%k,self.counter[k])
+    for k in sorted(HTTP_CODES.keys()):      
+      printer("code%s"%k,self.counter[str(k)])
      
 is_config=(len(sys.argv)>1 and sys.argv[1]=='config')
 files=getparams_from_config()
+
 limit=getlimit()
 
 printer=print_data
@@ -105,20 +113,20 @@ if len(files)<1:
 else:     
   for title,group,filename in files:
     #Aggr init
-    aggr=LatencyAggregator()
+    aggr=LatencyAggregator(title,group)
     #aggr_counters=Counter(dict([(str(i),0) for i in INTERVALS]+[('others',0)]))
 
     #Bots init
-    bots=BotsCounter()    
+    bots=BotsCounter(title,group)    
     
     #Http init
-    httpcodes=HttpCodesCounter()
+    httpcodes=HttpCodesCounter(title,group)
            
     #read from files valid rows
     fi=open(filename,'r')
     for row in fi:
       datas=RowParser(row)
-           
+
       if datas.get_date()>limit:                      
         #aggr evaluate
         aggr.update_with(datas)
@@ -132,19 +140,19 @@ else:
     #Aggr prints
     print "multigraph nginx_%s_%s"%('aggr',filename.replace('/','_').replace('.','_'))
     if is_config:
-      aggr.print_config_header(title,group)    
+      aggr.print_config_header()    
     aggr.print_data(printer)
         
     #Bots prints
     print "multigraph nginx_%s_%s"%('bots',filename.replace('/','_').replace('.','_'))
     if is_config:
-      bots.print_config_header(title,group)
+      bots.print_config_header()
     bots.print_data(printer)
           
     #Http prints
     print "multigraph nginx_%s_%s"%('http',filename.replace('/','_').replace('.','_'))
     if is_config:
-      httpcodes.print_config_header(title,group)
+      httpcodes.print_config_header()
     httpcodes.print_data(printer)
 
 
