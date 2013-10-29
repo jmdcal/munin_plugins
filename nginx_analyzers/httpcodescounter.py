c252bb887b332e9feb4fe916c9a4094a87d82137
Federico C. Guizzardi <cippinofg@gmail.com>
2013-10-29 14:33:52 +0100

    Fixed labels

--- nginx_analyzers/httpcodescounter.py
+++ nginx_analyzers/httpcodescounter.py
@@ -9,7 +9,7 @@ class HttpCodesCounter(BaseCounter):
   
   def __init__(self,title,group):
     super(HttpCodesCounter,self).__init__("Nginx http codes: %s"%title,group)
-    self.label="q.ty"
+    self.label="q.ty in %s mins"%MINUTES
     self.counter=Counter(dict([(str(i),0) for i in HTTP_CODES.keys()]))
     
   def update_with(self,datas):
@@ -18,7 +18,7 @@ class HttpCodesCounter(BaseCounter):
       self.counter[code]=self.counter[code]+1
               
   def print_data(self, printer):
-    for k in sorted(HTTP_CODES.keys()):      
+    for k,l in sorted(HTTP_CODES.items()):      
       printer(id="code%s"%k,
               value=self.counter[str(k)],
-              label="Number of %s in %s mins"%(k,MINUTES),)
+              label="[%s] %s "%(k,l),)
