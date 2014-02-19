ad2b47adb9fb892d8001a28525aa2848af28ed72
Federico C. Guizzardi <cippinofg@gmail.com>
2014-02-19 11:20:39 +0100

    Added support for data folders (cache, config, config_ext)

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '4.0.0'
+version = '4.0.1'
 
-setup(name='munin_plugins',
+name='munin_plugins'
+
+current=os.path.abspath(os.path.dirname(__file__))
+  
+config_dir='config'
+config_path=os.path.join(current,name,config_dir)
+config_files=[os.path.join(name,config_dir,f) for f in os.listdir(config_path)]
+
+config_ext_dir='config_ext'
+config_ext_path=os.path.join(current,name,config_ext_dir)
+config_ext_files=[os.path.join(name,config_ext_dir,f) for f in os.listdir(config_ext_path)]
+
+cache_dir='cache'
+cache_path=os.path.join(current,name,cache_dir)
+cache_files=[os.path.join(name,cache_dir,f) for f in os.listdir(cache_path)]
+
+setup(name=name,
       version=version,
       description="Sensors for munin",
       long_description=open('README').read(),
@@ -20,6 +36,9 @@ setup(name='munin_plugins',
       url='https://github.com/cippino/munin_plugins',
       license='LICENSE.txt',
       packages=find_packages(),
+      data_files = [(os.path.join(sys.prefix,'var',name,config_dir) ,config_files),
+                    (os.path.join(sys.prefix,'var',name,config_ext_dir) ,config_ext_files),
+                    (os.path.join(sys.prefix,'var',name,cache_dir) ,cache_files),],
       include_package_data=True,
       zip_safe=True,
       install_requires=[
@@ -34,3 +53,5 @@ setup(name='munin_plugins',
         ]
       },
 )
+        
+
