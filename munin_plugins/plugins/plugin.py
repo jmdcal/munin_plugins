9a1d2aeb75a2786686307528b4f40b6dd1615f3f
Federico C. Guizzardi <cippinofg@gmail.com>
2014-08-20 12:20:09 +0200

    Added metadata

--- munin_plugins/plugins/plugin.py
+++ munin_plugins/plugins/plugin.py
@@ -1,3 +1,17 @@
+"""
+
+This file is a part of Munin_Plugin project.
+You can find any information you like at urls
+
+https://pypi.python.org/pypi/munin_plugins
+https://github.com/cippino/munin_plugins
+
+"""
+
+__author__ = 'Federico C. Guizzardi cippinofg_at_gmail_com'
+__copyright__ = 'Copyright 2014, Federico C. Guizzardi'
+__license__ = 'GPL Version 2.0'
+
 import sys
 
 from os.path import join
--- munin_plugins/plugins/snsr_apache.py
+++ munin_plugins/plugins/snsr_apache.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python2.7
 
+"""
+
+This file is a part of Munin_Plugin project.
+You can find any information you like at urls
+
+https://pypi.python.org/pypi/munin_plugins
+https://github.com/cippino/munin_plugins
+
+"""
+
+__author__ = 'Federico C. Guizzardi cippinofg_at_gmail_com'
+__copyright__ = 'Copyright 2014, Federico C. Guizzardi'
+__license__ = 'GPL Version 2.0'
+
 import subprocess
 import re
 import sys
