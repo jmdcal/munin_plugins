a30af37f8889d575305710cad3e85328de3128b8
Federico C. Guizzardi <cippinofg@gmail.com>
2014-09-08 15:34:07 +0200

    Added time range limit

--- munin_plugins/plugins/snsr_errfiles.py
+++ munin_plugins/plugins/snsr_errfiles.py
@@ -18,6 +18,7 @@ import re
 import subprocess
 import cgi
 import sys
+from datetime import datetime
 
 from collections import Counter
 
@@ -34,8 +35,11 @@ class Errfiles(Plugin):
     inherit_env.update({
       'title':'Error counter',
       'group':'file',
+      'minutes':5,
       'file_0':"/var/log/syslog",
       'filter_0':"(.*)(ERROR|error)(.*)",
+      'date_filter_0':'^(\w{3}\s(\s|\d)\d\s+\d{2}:\d{2}:\d{2})',
+      'date_converter_0':"%b %d %H:%M:%S",
       'warning_0':100,
       'critical_0':1000,
     })
@@ -44,25 +48,33 @@ class Errfiles(Plugin):
   def get_files(self):
     files=self.getenv_prefix_with_id('file_')
     filters=dict(self.getenv_prefix_with_id('filter_'))
+    df=dict(self.getenv_prefix_with_id('date_filter_'))
+    dc=dict(self.getenv_prefix_with_id('date_converter_'))    
     w=dict(self.getenv_prefix_with_id('warning_'))
     c=dict(self.getenv_prefix_with_id('critical_'))    
-    return [(ff,filters.get(id,'undef'),w.get(id,0),c.get(id,0)) for id,ff in files]    
+    return [(ff,filters.get(id,'undef'),df.get(id,None),dc.get(id,None),w.get(id,0),c.get(id,0)) for id,ff in files]    
   
   def _fn_to_id(self,fn):
     return fn.strip().replace(' ','_').replace('/','_')
+  
+  def _fix_year(self,dt):
+    ty=datetime.now().year    
+    if dt.year!=ty and dt.year!=ty-1:
+      dt=dt.replace(ty)      
+    return dt  
   
   def print_config(self):
     print "graph_title %s"%self.getenv('title')
     print "graph_args --base 1000"
     print "graph_vlabel number"
     print "graph_category %s"%self.getenv('group')
-    for ff,flt,w,c in self.get_files():
+    for ff,flt,df,dc,w,c in self.get_files():
       id=self._fn_to_id(ff)
       print "%s.label %s" % (id,ff)
       print "%s.draw AREASTACK" % id
       print "%s.info %s"%(id,cgi.escape(flt))
       print "%s.warning %s"%(id,w)
-      print "%s.critical %s"%(id,c)
+      print "%s.critical %s"%(id,c)  
   
   def main(self,argv=None, **kw): 
     if self.check_config(argv):
@@ -72,23 +84,23 @@ class Errfiles(Plugin):
       if len(files)==0:
         sys.stderr.write('Not configured: see documentation\n')
       else:
-        for ff,flt,w,c in files:
+        limit=self.getlimit(self.getenv('minutes'))
+        for ff,flt,df,dc,w,c in self.get_files():
           id=self._fn_to_id(ff)
           count=0
           matcher=re.compile(flt)
+          dmatcher=re.compile(df)
           try:                        
             with open(ff,'r') as fi:
               for row in fi:
-                if matcher.match(row):
+                ds=dmatcher.search(row).group()
+                dt=self._fix_year(datetime.strptime(ds,dc))                                
+                if dt>limit and matcher.match(row):
                   count+=1
           except IOError:
             sys.stderr.write('NotExists: file %s not exists!\n'%filename)
           print "%s.value %s"%(id,count)
       
-      
-
-  
-
 def main(argv=None, **kw): 
   Errfiles().main()
 
